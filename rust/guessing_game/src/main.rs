43e7187f70763de0b4e3d0918b7e09b29e814b29
Tristan <tristanbarlowg@gmail.com>
2020-06-07 21:38:58 +0100

    Added guess number to the game

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,20 +1,20 @@
 use rand;
-use std::collections;
 use std::io;
 
-fn tryGuess(correct: &str) {
+fn try_guess(correct: &str, tries: i8) -> i8 {
     let mut guess = String::from("");
-
     println!("Please input your guess.");
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
     println!("You guessed: {}", guess);
 
+    let new_tries = tries + 1;
     if guess.trim() != correct {
         println!("The guess was wrong");
-        tryGuess(correct);
+        return try_guess(correct, new_tries);
     }
+    return new_tries;
 }
 
 fn main() {
@@ -22,11 +22,15 @@ fn main() {
 
     let mut rng = rand::thread_rng();
 
-    let x = rand::Rng::gen_range(&mut rng, 0, words.len());
+    for a in &words {
+        println!("{}", a);
+    }
 
+    let x = rand::Rng::gen_range(&mut rng, 0, words.len());
     let right_word = words[x];
 
-    println!("CORRECT {}", right_word);
-    tryGuess(right_word);
-    println!("CORRECT {}", right_word)
+    let i: i8 = 0;
+    let guesses = try_guess(right_word, i);
+    println!("CORRECT!");
+    println!("IT TOOK YOU {} GUEESES", guesses);
 }
