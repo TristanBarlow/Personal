e6a060940d8988ddf19db120a72bfcbd801f7053
Tristan Barlow <tristanbarlowg@gmail.com>
2020-06-08 14:00:35 +0100

    fixed the bugger reading and added some html

--- rust/webserver/src/main.rs
+++ rust/webserver/src/main.rs
@@ -1,5 +1,7 @@
 use std::fs;
 use std::io::prelude::*;
+use std::io::BufReader;
+use std::io::Cursor;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
@@ -16,12 +18,13 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream, html: &String) {
-    let mut buffer = [0; 512];
+    let mut request = String::new();
+    let mut b = BufReader::new(&stream);
 
-    stream.read(&mut buffer).unwrap();
-
+    while !b.read_line(&mut request).is_ok() {}
+    println!("{}", request);
     let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", html);
 
-    stream.write(response.as_bytes()).unwrap();
+    stream.write_all(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
